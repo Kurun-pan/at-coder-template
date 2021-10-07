2faab6468ff56991f3d4600a01219b0a60195f6d
Kurun <kurunpan09@gmail.com>
2021-10-07 20:56:54 +0900

    Update

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [[ $# != 1 ]]; then
     echo "対象の問題番号を指定してね [a-f]"
     exit 1
 else
-    clang++ $1.cc -o test -std=c++11 -stdlib=libc++
+    clang++ $1.cc -o test -std=c++14 -stdlib=libc++
     ./test    
     rm ./test
 fi
